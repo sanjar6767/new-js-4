8123ebe55ba0a026eef6c5d2da42829b4751099f
sanjar6767 <rasulberdievsanjar@gmail.com>
2025-10-25 19:28:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let son = prompt("son kiriting")
+
+console.log(son)
+
+if (son > 30) {
+    console.log("bu son 30 katta")
+}
+else if(son == 30){
+    console.log("bu son 30")
+}
+
+else{
+    console.log("bu son 30 dan kichik")
+}
